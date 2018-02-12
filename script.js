22c93eeea1ecc132675136ed93b41ea223f4eb6a
Josiel <josielalv@hotmail.com>
2018-02-12 19:33:56 -0200

    criando cobra, comida

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Configurações iniciais
+var cobraX = 2;
+var cobraY = 2;
+
 var height = 23;
 var width = 35;
 
@@ -8,13 +12,14 @@ function run() {
 
 function iniciar() {
     criarMapa();
+    criarCobra();
+    criarComida();
 }
 
-//Criando Mapa
+//Criando mapa
 function criarMapa() {
     var baseJogo = document.getElementById("baseJogo");
     var html = "<table>";
-    // document.write("<table>");
     for (var i = 0; i < height; i++) {
         html += "<tr>";
         for (var j = 0; j < width; j++) {
@@ -30,6 +35,42 @@ function criarMapa() {
     baseJogo.innerHTML = html;
 }
 
+//Criando cobra
+function criarCobra() {
+    set(cobraX, cobraY, "cobra");
+}
+
+//Pegando posição na tabela
+function get(x, y) {
+    return document.getElementById(x + "-" + y);
+}
+
+//Setando posição na tabela
+function set(x, y, classe) {
+    get(x, y).setAttribute("class", classe);
+}
+
+//Pega o tipo da posição, se é parede ou vazio
+function getClasse(x, y) {
+    console.log(x);
+    console.log(y);
+    return get(x, y).getAttribute("class");
+}
+
+//Criando comida
+function criarComida() {
+    var colocado = false;
+    while (!colocado) {
+        var comidaX = Math.floor(Math.random() * (width - 1) + 1);
+        var comidaY = Math.floor(Math.random() * (height - 1) + 1);
+        //Verifica se a comida vai ser colocada em um lugar vazio (fora da cobra)
+        if (getClasse(comidaX, comidaY) == "vazio") {
+            colocado = true;
+        }
+    }
+    //Coloca comida na tela
+    set(comidaX, comidaY, "comida");
+}
 window.onload = function () {
     run();
 }
