e26a1f3a641063e8e029ae01fa2784f729de7eb0
Josiel <josielalv@hotmail.com>
2018-02-12 22:09:18 -0200

    movimento

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 //Configurações iniciais
 var cobraX = 2;
 var cobraY = 2;
-
+var raboX = [cobraX];
+var raboY = [cobraY];
 var height = 23;
 var width = 35;
+
+var tamanho = 0;
+var cX;
+var cY;
+
+var jogando = false;
+var gameOver = false;
+// cima = 0 ; baixo = 1; esquerda = 2; direita = 3
+var direcao = 3;
+var intervalo = 100;
+var incremento = 1;
+var int;
+
 
 //Inicio do jogo
 function run() {
     iniciar();
+    int = setInterval(gameLoop, intervalo);
 }
 
 function iniciar() {
@@ -47,13 +62,13 @@ function get(x, y) {
 
 //Setando posição na tabela
 function set(x, y, classe) {
-    get(x, y).setAttribute("class", classe);
+    if (x != null && y != null) {
+        get(x, y).setAttribute("class", classe);
+    }
 }
 
 //Pega o tipo da posição, se é parede ou vazio
 function getClasse(x, y) {
-    console.log(x);
-    console.log(y);
     return get(x, y).getAttribute("class");
 }
 
@@ -70,7 +85,75 @@ function criarComida() {
     }
     //Coloca comida na tela
     set(comidaX, comidaY, "comida");
+    cX = comidaX;
+    cY = comidaY;
 }
+
+//Verifica tecla pressionada e a direção que a cobra irá seguir
+window.addEventListener("keypress", function key() {
+    var tecla = event.keyCode;
+    console.log(tecla);
+    //Para cima (W ou seta para cima)
+    if (direcao != 1 && (tecla == 119 || tecla == 87 || tecla == 38)) {
+        direcao = 0;
+    }
+    //Para baixo (S ou seta para baixo)
+    else if (direcao != 0 && (tecla == 115 || tecla == 83 || tecla == 40)) {
+        direcao = 1;
+    }
+    //Para esquerda (A ou seta para esquerda)
+    else if (direcao != 3 && (tecla == 97 || tecla == 65 || tecla == 37)) {
+        direcao = 2;
+    }
+    //Para direita (D ou seta para direita)
+    else if (direcao != 2 && (tecla == 100 || tecla == 68 || tecla == 39)) {
+        direcao = 3;
+    }
+
+    if (!jogando) {
+        jogando = true;
+    } else if (tecla == 32) {
+        jogando = false;
+    }
+});
+
+//Checa o estado do jogo 
+function gameLoop() {
+    if (jogando && !gameOver) {
+        atualiza();
+    } else if (gameOver) {
+        clearInterval(int);
+    }
+}
+
+//Atualiza a posição da cobra e do rabo
+function atualiza() {
+    atualizaRabo();
+    set(raboX[tamanho], raboY[tamanho], "vazio");
+    if (direcao == 0) {
+        cobraY--;
+    } else if (direcao == 1) {
+        cobraY++;
+    } else if (direcao == 2) {
+        cobraX--;
+    } else if (direcao == 3) {
+        cobraX++;
+    }
+    set(cobraX, cobraY, "cobra");
+}
+
+function atualizaRabo() {
+    for (var i = tamanho; i > 0; i--) {
+        raboX[i] = raboX[i - 1];
+        raboY[i] = raboY[i - 1];
+    }
+    raboX[0] = cobraX;
+    raboY[0] = cobraY;
+}
+
 window.onload = function () {
     run();
 }
+
+// 0 = cima; 1 = baixo; 2 = esquerda; 3 = direita
+// 0 = up; -1 = down; 1 = esquerda; direita = 2 tuto
